755b62e8cbe0e682df8d4e4a841fb8e56af3bc8f
robertoraciti <rob.raciti97@gmail.com>
2023-11-07 16:58:53 +0100

    Milestone 4

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,10 +8,32 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $projects = Project::select('id', 'typology_id', 'name', 'repo')
             ->paginate(9);
+
+        return response()->json($projects);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $projects = Project::select('id', 'typology_id', 'name', 'repo', 'collaborators', 'publishing_date')
+            ->where('id', $id)
+            ->first();
+
 
         return response()->json($projects);
     }
